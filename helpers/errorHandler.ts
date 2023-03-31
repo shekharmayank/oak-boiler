b47bd813ba58312541ea464e357f66be2df54a65
Mayank Shekhar <mayankshekhar03@gmail.com>
2023-03-31 21:58:31 +0530

    Basics

--- helpers/errorHandler.ts
+++ helpers/errorHandler.ts
@@ -1,23 +1,18 @@
 import { isHttpError } from "https://deno.land/std@0.178.0/http/http_errors.ts";
+import { renderFile } from "https://deno.land/x/eta@v1.11.0/file-handlers.ts";
 import { Context } from "https://deno.land/x/oak@v12.1.0/context.ts";
-import { Status } from "https://deno.land/x/oak@v12.1.0/deps.ts";
 
 export async function errorHandler(ctx: Context, next: () => Promise<unknown>) {
   try {
     await next();
   } catch (err) {
     if (isHttpError(err)) {
-      switch (err.status) {
-        case Status.InternalServerError:
-          ctx.response.status = 500;
-          ctx.response.body = '500 Internal Server Error';
-          break;
-        default:
-          // handle other statuses
-      }
+      const templateResult = await renderFile("./error", { status: err.status, msg: err.message });
+      ctx.response.status = err.status;
+      ctx.response.body = templateResult;
     } else {
-      // rethrow if you can't handle the error
-      throw err;
+      const templateResult = await renderFile("./error", { status: 'Unknown error', msg: "Not sure what just happened!" });
+      ctx.response.body = templateResult;
     }
   }
 }
--- index.ts
+++ index.ts
@@ -1,23 +1,45 @@
 import { Application } from "https://deno.land/x/oak@v12.1.0/mod.ts";
-import { errorHandler } from "./helpers/errorHandler.ts";
 import { mainRouter } from "./routes/index.ts";
 import { renderFile, configure } from "https://deno.land/x/eta@v1.11.0/mod.ts"
+import { errorHandler } from "./helpers/errorHandler.ts";
 
 const app = new Application();
 
-const viewPath = `${Deno.cwd()}/views/`
+/**
+ * Registering error handler
+ */
+app.use(errorHandler);
 
+/**
+ * Configuring `eta` engine
+ */
+const viewPath = `${Deno.cwd()}/views/`;
 configure({
   views: viewPath
-})
+});
 
+/**
+ * Main router
+ */
 app.use(mainRouter.routes());
 
+/**
+ * 404 page
+ */
 app.use(async (ctx) => {
-  const templateResult = await renderFile("./error", { status: 404, msg: "Page not found"});
+  const templateResult = await renderFile("./error", { status: 404, msg: "Page not found" });
   ctx.response.status = 404;
   ctx.response.body = templateResult;
 });
-app.use(errorHandler);
 
+/**
+ * For uncaught exceptions
+ */
+app.addEventListener("error", (evt) => {
+  console.log(evt.error.message);
+});
+
+app.addEventListener('listen', ({ hostname, port, secure }) => {
+  console.log(`✨ App running: ${secure ? 'https://' : 'http://'}${hostname ?? 'localhost'}:${port} ✨`)
+});
 await app.listen({ port: 8000 });
